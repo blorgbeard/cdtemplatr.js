361db793bb4f8d742b7f53eed4ec06123fe97baa
Philip du Fresne <philip.du.fresne@vista.co>
2016-06-10 09:13:18 +1200

    react works

---
+++ app/main.jsx
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+function render() {
+    ReactDOM.render(<span>This is react, kinda.</span>, document.getElementById("container"));
+}
+
+render();
