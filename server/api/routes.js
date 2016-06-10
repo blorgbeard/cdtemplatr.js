74e22a2e115690162514873be66423fdae9d724a
Yan <yan.liu@vista.co>
2016-06-10 15:49:49 +1200

    Added some outline for newArtefacts and removedArtefacts

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,5 +1,6 @@
 var router = require("express").Router();
 
+// Builds
 router.route('/builds').get(getBuilds);
 
 function getTag() {
@@ -12,6 +13,21 @@ function getBuilds(req, res) {
     {key: 2, name: "Cinema", status:"SNAFU", tag: getTag()},
     {key: 3, name: "HeadOffice", status:"OK", tag: getTag()},
     {key: 4, name: "FilmProgramming", status:"32 additions / 3 deletions", tag: getTag()},
+  ];
+  res.json(data);
+}
+
+// New Artefacts
+router.route('/newArtefacts').get(GetNewArtefacts);
+
+function GetNewArtefacts(req, res){
+  var data = [
+    {key: 1, name: "\\_Install_Customisation\\Customers\\Brazil\\Domain\\Vista.Circuit.Domain.Brazil\\bin\\Vista.Brazil.TaxNumber.Common.dll"},
+    {key: 2, name: "\\_Install_Customisation\\Customers\\Brazil\\Domain\\Vista.Circuit.Domain.Brazil\\bin\\Vista.Environment.OnPremise.FileUtilities.dll"},
+    {key: 3, name: "\\_Install_Customisation\\Customers\\Brazil\\Domain\\Vista.Circuit.Domain.Brazil\\bin\\Vista.KeyValueStore.Extended.dll"},
+    {key: 4, name: "\\_Install_Customisation\\Customers\\Brazil\\Vista.Brazil.AccountantMaint\\bin\\Vista.Brazil.TaxNumber.Common.dll"},
+    {key: 5, name: "\\_Install_Customisation\\Customers\\Brazil\\Vista.Brazil.AccountantMaint\\bin\\Vista.Environment.OnPremise.FileUtilities.dll"},
+    {key: 6, name: "\\_Install_Customisation\\Customers\\Brazil\\Vista.Brazil.AccountantMaint\\bin\\Vista.KeyValueStore.Extended.dll"}
   ];
   res.json(data);
 }
