9339b3370edc28b98c1895c59c68c6925a0b3e86
Yan <yan.liu@vista.co>
2016-06-13 11:05:49 +1200

    Renamed NewArtefactsTable to ArtefactsTable, using it for both new and removed artefacts

--- app/main.jsx
+++ app/main.jsx
@@ -3,25 +3,12 @@ var ReactDOM = require("react-dom");
 
 var BuildList = require("./components/BuildList.jsx");
 
-var NewArtefactsTable = require("./components/NewArtefactsTable.jsx")
-
-var RemovedArtefactsTable = React.createClass({
-  render: function(){
-    return (
-      <table className="table table-hover">
-        <thead><tr>
-          <th>Artefacts removed from CD</th>
-        </tr></thead>
-        <tbody></tbody>
-      </table>
-    )
-  }
-})
+var ArtefactsTable = require("./components/ArtefactsTable.jsx")
 
 function render() {
   ReactDOM.render(<BuildList url="/api/builds"/>, document.getElementById("buildListContainer"));
-  ReactDOM.render(<NewArtefactsTable url="/api/newArtefacts" pollInterval={5000}/>, document.getElementById("newArtefactsContainer"));
-  ReactDOM.render(<RemovedArtefactsTable url="/api/builds" pollInterval={5000}/>, document.getElementById("removedArtefactsContainer"));
+  ReactDOM.render(<ArtefactsTable url="/api/newArtefacts" tableHeading="New artefacts in CD"/>, document.getElementById("newArtefactsContainer"));
+  ReactDOM.render(<ArtefactsTable url="/api/removedArtefacts" tableHeading ="Artefacts removed from CD"/>, document.getElementById("removedArtefactsContainer"));
 }
 
 render();
