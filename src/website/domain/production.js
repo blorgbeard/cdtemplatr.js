f59ff1596a7dd4d4b4510500fe197b64ec5cc485
Philip du Fresne <philip.du.fresne@vista.co.nz>
2016-09-05 13:21:05 +1200

    Remove old, unused code.

--- src/website/domain/production.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var buildController = require('./production/buildController.js');
-
-module.exports = {
-  getBuilds: buildController.getList,
-  getBuildDetails: buildController.getDetails,
-  approveChanges: null
-};
--- src/website/domain/testing.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var buildController = require('./testing/buildController.js');
-
-module.exports = {
-  getBuilds: buildController.getBuilds,
-  getBuildDetails: buildController.getBuildDetails,
-  approveChanges: buildController.approveChanges
-};
