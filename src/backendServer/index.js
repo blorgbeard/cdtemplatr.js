903913b7c0bcfc34ad52e6c09e76b953db2716ee
Philip du Fresne <philip.du.fresne@vista.co>
2016-10-12 10:24:45 +1300

    Add ping function to backend server

--- src/backendServer/index.js
+++ src/backendServer/index.js
@@ -10,14 +10,21 @@ var app = express();
 
 var Promise = require('bluebird');
 
-Promise.all([    
+Promise.all([
   requireShared('Domain')(config),
-  requireShared('Tfs')(config)  
+  requireShared('Tfs')(config)
 ]).then(results => {
 
   var controller = require('./build/Controller')(results[0], results[1]);
   var router = require('./build/Router')(controller);
   app.use('/build', router);
+
+  app.get('/ping', function(req,res) {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    res.status(200).send('pong');
+  });
 
   var port = config.backendServer.port || 7778;
 
